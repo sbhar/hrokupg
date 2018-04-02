40ef5b1e0227700c32afbd141a45018e31594444
sugata <sugatabhar@yahoo.com>
2018-04-02 17:24:06 +0530

    Corrected code

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+//$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+$result = pg_query($pg_conn, "SELECT * from COMPANY");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
-  print "Tables in your database:\n";
-  $ret = pg_query($db, $sql);
-  while($row = pg_fetch_row($ret)) {
+  //print "Tables in your database:\n";
+  while($row = pg_fetch_row($result)) {
       echo "ID = ". $row[0] . "<br/>";
       echo "NAME = ". $row[1] ."<br/>";
       echo "ADDRESS = ". $row[2] ."<br/>";
