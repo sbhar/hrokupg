b827d3dc9c0620bc5ecae06feb4df7ee8df57b97
sugata <sugatabhar@yahoo.com>
2018-04-03 14:03:16 +0530

    Corrected becoz port was missing

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   //return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-  return "pgsql://$user:$pass@$host/" . substr($path, 1) . "/";
+  return "pgsql://$user:$pass@$host:5432/" . substr($path, 1) . "/";
 }
-echo pg_connection_string_from_database_url();
+//echo pg_connection_string_from_database_url();
 $dsn = pg_connection_string_from_database_url();
 $clients = [];
 
